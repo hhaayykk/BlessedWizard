61969b56cea31eddc09544318ad07ff7c4df99d9
Hayk <77168220+hhaayykk@users.noreply.github.com>
2026-01-22 23:57:11 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
 // Firebase Configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyA5GBWuPISCq-EmLXB5YN0Q-QOSZucbifo",
-  authDomain: "blessedwizard-6d357.firebaseapp.com",
-  projectId: "blessedwizard-6d357",
-  storageBucket: "blessedwizard-6d357.firebasestorage.app",
-  messagingSenderId: "174002490679",
-  appId: "1:174002490679:web:746f335a008216d4675eb2",
-  measurementId: "G-71YTZSZXY6"
+  apiKey: "###",
+  authDomain: "###",
+  projectId: "###",
+  storageBucket: "###",
+  messagingSenderId: "###",
+  appId: "###",
+  measurementId: "###"
 };
-
-// Telegram Bot Configuration
-export const TELEGRAM_BOT_TOKEN = "8417592854:AAHYll3iNtfdBjh9q3q5yiZ3o0OtsTh-tMQ";
-export const TELEGRAM_GROUP_CHAT_ID = "-1003226470463";
-export const BOT_USERNAME = "BlessedWizardBot";
